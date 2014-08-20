e6e8fe814b9cf520cbc7a42dd740d067359eb577
Drew Botka <hellodrewbot@gmail.com>
2014-08-20 16:14:47 -0400

    added basic nav and included logo font

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,25 +6,33 @@
 $('.second').waypoint(function(direction) {
 	if (direction === 'down') {
 		$('.box-two').addClass('box-two-move');
-  		console.log('second div down 80%');
 	} else {
 		$('.box-two').removeClass('box-two-move');
-  		console.log('second div up 80%');
 	}
 }, { offset: '80%' });
 
 $('.second').waypoint(function(direction) {
 	if (direction === 'down') {
 		$('.box-two').removeClass('box-two-move');
-  		console.log('second div down 20%');
 	} else {
 		$('.box-two').addClass('box-two-move');
-  		console.log('second div up 20%');
 	}
-}, { offset: '20%' });
+}, { offset: '0%' });
 
 
 ////////////////////////////////////////////////// Third div
-$('.third').waypoint(function() {
-  console.log('third div.');
-}, { offset: '90%' });
+$('.third').waypoint(function(direction) {
+	if (direction === 'down') {
+		$('.box-three').addClass('box-two-move');
+	} else {
+		$('.box-three').removeClass('box-two-move');
+	}
+}, { offset: '70%' });
+
+$('.third').waypoint(function(direction) {
+	if (direction === 'down') {
+		$('.box-three').removeClass('box-two-move');
+	} else {
+		$('.box-three').addClass('box-two-move');
+	}
+}, { offset: '0%' });

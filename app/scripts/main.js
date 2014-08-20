1c43283b2e340dc3fc40fc4ac32944d5a95b08ae
Drew Botka <hellodrewbot@gmail.com>
2014-08-19 23:09:34 -0400

    scroll functions work on up and down direction now

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,30 @@
 
 
 ////////////////////////////////////////////////// Way points
-$('.second').waypoint(function() {
-  $('.box-two').addClass('box-two-move');
-  console.log('second div.');
+
+////////////////////////////////////////////////// Second div
+$('.second').waypoint(function(direction) {
+	if (direction === 'down') {
+		$('.box-two').addClass('box-two-move');
+  		console.log('second div down 80%');
+	} else {
+		$('.box-two').removeClass('box-two-move');
+  		console.log('second div up 80%');
+	}
 }, { offset: '80%' });
 
-$('.second').waypoint(function() {
-  $('.box-two').removeClass('box-two-move');
-  console.log('second div.');
+$('.second').waypoint(function(direction) {
+	if (direction === 'down') {
+		$('.box-two').removeClass('box-two-move');
+  		console.log('second div down 20%');
+	} else {
+		$('.box-two').addClass('box-two-move');
+  		console.log('second div up 20%');
+	}
 }, { offset: '20%' });
 
+
+////////////////////////////////////////////////// Third div
 $('.third').waypoint(function() {
   console.log('third div.');
 }, { offset: '90%' });

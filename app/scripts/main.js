3b02b635da3304e2cb11a0aa8174f9c36988e8f2
Drew Botka <hellodrewbot@gmail.com>
2014-08-22 19:19:51 -0400

    added back to top button to footer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,7 +9,13 @@ $('.home-button').click(function(){
 	$('.main-copy-headline').addClass('main-copy-headline-bounce');
     $('.main-copy-paragraph').addClass('main-copy-paragraph-bounce');
     $('.logo').addClass('logo-bounce');
+    $('.main-copy-headline').removeClass('main-copy-headline-bounce');
+    $('.main-copy-paragraph').removeClass('main-copy-paragraph-bounce');
     console.log('click worked')
+})
+
+$('.to-top').click(function(){
+	jQuery('html,body').animate({scrollTop:0},0);
 })
 
 
@@ -17,16 +23,30 @@ $('.home-button').click(function(){
 
 ////////////////////////////////////////////////// Way points
 
+////////////////////////////////////////////////// Main div
+
 ////////////////////////////////////////////////// Second div
+////////////////////////////////////////// transitions related to Main div triggered by second
+$('.second').waypoint(function(direction) {
+	if (direction === 'down') {
+		$('.main-copy-headline').removeClass('main-copy-headline-bounce');
+    	$('.main-copy-paragraph').removeClass('main-copy-paragraph-bounce');
+	} 
+}, { offset: '0%' })
+
+$('.second').waypoint(function(direction) {
+	if (direction === 'up') {
+		$('.main-copy-headline').addClass('main-copy-headline-bounce');
+    	$('.main-copy-paragraph').addClass('main-copy-paragraph-bounce');
+	} 
+}, { offset: '100%' })
+
+
 $('.second').waypoint(function(direction) {
 	if (direction === 'down') {
 		$('.box-two').addClass('box-two-move');
-		$('.main-copy-headline').removeClass('main-copy-headline-bounce');
-    	$('.main-copy-paragraph').removeClass('main-copy-paragraph-bounce');
 	} else {
 		$('.box-two').removeClass('box-two-move');
-		$('.main-copy-headline').addClass('main-copy-headline-bounce');
-    	$('.main-copy-paragraph').addClass('main-copy-paragraph-bounce');
 	}
 }, { offset: '50%' });
 
